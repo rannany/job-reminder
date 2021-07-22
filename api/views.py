a23e5ee3464894cb6438604fd8aec0d008fc74f0
Saint-Clair Rannany <rannany02@gmail.com>
2021-07-22 08:54:14 -0300

    Atualização a api de criação de tasks

--- api/views.py
+++ api/views.py
@@ -8,28 +8,49 @@ from api.serializers import TaskSerializer
 
 # Create your views here.
 
-class TaskViewSet(viewsets.ViewSet):
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.user
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_superuser
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
     authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
 
-    def list(self, request):
-        tasks = Task.objects.all()
-        if request.query_params.get('done') or None:
-            tasks = tasks.filter(is_done__exact=request.query_params.get('done') in ['True', 'true'] if True else False)
-        if request.query_params.get('title') or None:
-            tasks = tasks.filter(title__icontains=request.query_params.get('title'))
-        serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
 
-    def retrieve(self, request, pk=None):
-        task = get_object_or_404(Task, pk=pk)
-        serializer = TaskSerializer(task)
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
-    def create(self, request):
-        serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'status': 'salvo com sucesso!'})
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class TaskViewSet(viewsets.ViewSet):
+#     authentication_classes = [authentication.TokenAuthentication]
+#     permission_classes = [permissions.IsAuthenticated, IsOwner]
+#
+#     def list(self, request):
+#         tasks = Task.objects.filter(user=request.user)
+#         if request.query_params.get('done') or None:
+#             tasks = tasks.filter(is_done__exact=request.query_params.get('done') in ['True', 'true'] if True else False)
+#         if request.query_params.get('title') or None:
+#             tasks = tasks.filter(title__icontains=request.query_params.get('title'))
+#         serializer = TaskSerializer(tasks, many=True)
+#         return Response(serializer.data)
+#
+#     def retrieve(self, request, pk=None):
+#         task = get_object_or_404(Task, pk=pk)
+#         serializer = TaskSerializer(task)
+#         return Response(serializer.data)
+#
+#     def create(self, request):
+#         serializer = TaskSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({'status': 'salvo com sucesso!'})
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
